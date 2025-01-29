db3a3486f97676e976957513c2748efc0a399b33
nikkiarae <nikkiandrewrae@gmail.com>
2025-01-29 13:05:13 +0000

    Added env file

--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,13 @@ module.exports = function (environment) {
   };
 
   ENV.firebase = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
   };
 
   if (environment === 'test') {
